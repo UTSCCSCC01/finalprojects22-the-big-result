c878f4c149e30f28dce76c582e6e28938828e896
Carlos Huang <carlos@smilecdr.com>
2022-05-24 16:01:55 -0400

    slight modification to connection string for connecting to azure DB

--- backend/dbConnection.py
+++ backend/dbConnection.py
@@ -5,6 +5,6 @@ import os
 def sampleQuery():
     load_dotenv("./config/.env")
     DB_password = os.environ.get("DATABASE_PASSWORD")
-    engine = create_engine(f"mssql+pyodbc://masterUsername:{DB_password}@my-database-csc-c01.database.windows.net:1433/csc-c01-db?driver=ODBC+Driver+17+for+SQL+Server")
+    engine = create_engine(f"mssql+pyodbc://masterUsername:{DB_password}@my-database-csc-c01.database.windows.net:1433/my-database-csc-c01?driver=ODBC+Driver+17+for+SQL+Server")
 
     return {"myquery": str(engine.execute("SELECT * from Persons").fetchall())}
