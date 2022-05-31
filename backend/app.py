3a617da446eaabd414969d61e31b43bc097105b8
Carlos Huang <carlos@smilecdr.com>
2022-05-31 17:33:27 -0400

    slight changes to app to get sqlalchemy to work, removed learning folder

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,51 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import time
+from dotenv import load_dotenv
+import os
 
 from dbConnection import sampleQuery
 
 from sampleFeature.mySampleFeature import sampleBlueprint
-
-
 from flask_sqlalchemy import SQLAlchemy
+
+
+def getDBURL() -> str:
+    load_dotenv(f".{os.sep}config{os.sep}.env")
+    DB_password = os.environ.get("DATABASE_PASSWORD")
+    return f"mssql+pyodbc://masterUsername:{DB_password}@my-database-csc-c01.database.windows.net:1433/my-database-csc-c01?driver=ODBC+Driver+17+for+SQL+Server"
+
 
 app = Flask(__name__)
 app.register_blueprint(sampleBlueprint, url_prefix='/example')
 CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = getDBURL()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
 @app.route("/")
 def hello_world():
-    return {"hello":'5'}
+    return {"hello": '5'}
+
 
 @app.route("/time", methods=['GET'])
 def get_time():
     return jsonify({'time': time.time()})
 
-@app.route("/db", methods= ['GET'])
+
+@app.route("/db", methods=['GET'])
 def getSampleQuery():
     return sampleQuery()
 
 
+@app.route("/stuff", methods=['GET'])
+def databaseTestingStuff():
+    return str(db.engine.execute("SELECT * from Persons").fetchall())
 
 
 if __name__ == "__main__":
+    # print(getDBURL())
     app.run(debug=True)
-
-
-
