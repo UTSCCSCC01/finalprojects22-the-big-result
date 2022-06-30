bcfbcae6f36e7734bb86e55584f2a6b731b562e5
Vishal Deb Sahoo <76522645+vishal-sahoo@users.noreply.github.com>
2022-06-30 18:39:09 -0400

    Create initial front-end to display list of past bookings and set-up connection to back-end

---
+++ frontend/src/components/Bookings/BookingCustomerPast.jsx
@@ -0,0 +1,39 @@
+import "./Booking.css";
+import "../Card.css";
+
+function BookingCustomerPast(props) {
+    return (
+      <div
+        onClick={() => (window.location = "/booking")}
+        className="booking-card card"
+      >
+        <div class="metadata">
+          <img src={props.picURL} alt={props.provider} />
+          <div className="metadata-text">
+            <h2 className="highlight">{props.service}</h2>
+            <h3>with {props.provider}</h3>
+            <p>on {props.date}</p>
+            <p>at {props.time}</p>
+            <p>for cost of ${props.cost}
+            </p>
+            {/* <p>{props.description}</p>   */}
+          </div>
+        </div>
+        <div className="desc">
+          <div className="review-holder">
+            {/* <p>{props.review}</p> */}
+          </div>
+          <div className="btn-group">
+            {/* {props.type =="Past" ? <button>Book Again</button> : <button>Reschedule</button>}
+            {props.type == "Past" ? <button>Leave a Review</button> : <button>Cancel</button>} */}
+            <button>Book Again</button>
+            {/* <button>Leave a Review</button> */}
+            {props.review == "" ? <button>Leave A Review</button> : <button>See My Review</button>}
+            <button className="transparent-btn">About Professional</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default BookingCustomerPast;
---
+++ frontend/src/views/CustPastBookingsPage.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import BookingCustomerPast from "../components/Bookings/BookingCustomerPast";
+
+function CustPastBookingsPage(props) {
+  // type = past
+  const [bookingsList, setBookingsList] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://127.0.0.1:5000/customerPastBookings",
+    })
+      .then((response) => {
+        const res = response.data;
+        setBookingsList(res.bookings);
+      })
+      .catch((err) => {
+        console.log("ERR");
+        console.log(err.response);
+      });
+  }, []);
+
+  return (
+    <div className="bookings-page">
+      <h1>Past Bookings</h1>
+      <div className="bookings">
+        {bookingsList.map((booking) => (
+          <BookingCustomerPast
+            provider={booking.provider}
+            service={booking.service}
+            review={booking.review}
+            cost={booking.cost}
+            picURL={booking.picURL}
+            date={booking.date}
+            time={booking.time}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CustPastBookingsPage;
