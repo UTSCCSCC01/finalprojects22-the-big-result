628e97ac25ab4bf5c001a9cb47418599ba4fdf71
Amy Yao <AmyXi.Yao@gmail.com>
2022-07-02 14:40:07 -0400

    THEB-17 Modify booking card display

--- frontend/src/components/Bookings/BookingProfessionalUpcoming.jsx
+++ frontend/src/components/Bookings/BookingProfessionalUpcoming.jsx
@@ -8,11 +8,11 @@ function BookingProfessionalUpcoming(props) {
         <img src={props.picURL} alt={props.service} />
         <div className="metadata-text">
           <h2 className="highlight">{props.service}</h2>
-          <h3>with {props.customer}</h3>
-          <p>on {props.date}</p>
-          <p>from {props.startTime} to {props.endTime}</p>
-          <p>at {props.location}</p>
-          <p>for cost of ${props.price}</p>
+          <h3>Customer: {props.customer}</h3>
+          <p>Date: {props.date}</p>
+          <p>Time: {props.startTime} to {props.endTime}</p>
+          <p>Location: {props.location}</p>
+          <p>Price: ${props.price}</p>
         </div>
       </div>
       <div className="desc">
