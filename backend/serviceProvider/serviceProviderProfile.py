36043b2f72df04c032e00a624dfbe6f872a466e7
Vishal Deb Sahoo <76522645+vishal-sahoo@users.noreply.github.com>
2022-07-02 18:11:10 -0400

    Connect services and provider profile to database

--- backend/serviceProvider/serviceProviderProfile.py
+++ backend/serviceProvider/serviceProviderProfile.py
@@ -1,29 +1,44 @@
 from flask import Blueprint, jsonify
+from DAOs import ProfessionalsDAO, CustomersDAO
 
 serviceProviderBlueprint = Blueprint("serviceProvider", __name__)
 
 @serviceProviderBlueprint.route("/")
 def getServiceProviderProfile():
-    return jsonify({
-        "name": "Steven Adams",
-        "rating": 4.5,
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
-        "felis imperdiet proin fermentum leo. Lobortis elementum nibh tellus molestie nunc non. Nullam non nisi est sit amet facilisis magna. Odio pellentesque",
-        "services": "Hairstyle",
-        "profilePictureLink": "https://picsum.photos/200",
-        "calendar": "Some calendar stuff here that we would probably need later on",
-        "reviews": getReviews(3)
-    })
+    
+    dao = ProfessionalsDAO()
+    professional = dao.getAllProfessionals()[0]
 
+    return {
+            "name": professional.lastName + ", " + professional.firstName,
+            "rating": professional.ratings,
+            "description": professional.description,
+            "services": getServices(professional.services),
+            "profilePictureLink": "https://picsum.photos/200",
+            "calendar": "Some calendar stuff here that we would probably need later on",
+            "reviews": getReviews(dao.getAllReviewsForProfesional(professional.id))
+        }
 
-def getReviews(numRevs = 3)-> list:
+def getServices(services):
+    result = ""
+    for service in services:
+        if result == "":
+            result = service.serviceName
+        else:
+            result = result + "," + service.serviceName
+    return result
+
+def getReviews(reviews, numRevs = 3) -> list:
     reviewList = []
-    for i in range(numRevs):
+    custDAO = CustomersDAO()
+
+    for i in range(min(len(reviews), numRevs)):
+        customer = custDAO.getCustomerOnID(reviews[i].customerID)
         reviewList.append({
-            "service": "hairstyle",
-            "reviewedBy": "Bob Marley",
-            "rating": 3.7,
-            "imageLink":"https://picsum.photos/100",
-            "reviewDescription":"elit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Posuere urna nec tincidunt praesent semper feugiat nibh sed. Gravida cum sociis natoque penatibus et magnis dis. Orci dapibus ultrices in iaculis."
+            "service": reviews[i].booking.serviceName,
+            "reviewedBy": customer.lastName + ", " + customer.firstName,
+            "rating": reviews[i].ratings,
+            "imageLink": "https://picsum.photos/100",
+            "reviewDescription": reviews[i].description
         })
     return reviewList
