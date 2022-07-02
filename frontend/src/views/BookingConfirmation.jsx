348d82e6ad9b873ce42abb64d84e624974d488ce
avaove <aavaa.oveisi@gmail.com>
2022-07-02 05:01:50 -0400

    THEB-8 started connecting booking confirmation with backend

--- frontend/src/views/BookingConfirmation.jsx
+++ frontend/src/views/BookingConfirmation.jsx
@@ -1,13 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MenuItem, FormControl, InputLabel, Select } from "@mui/material";
 import "../components/Form.css";
 
 function BookingConfirmation() {
   //separated these out into different states for ease, can combine into 1 if we want
-  const [service, setService] = useState("");
-  const [provider, setProvider] = useState("");
-  const [time, setTime] = useState("");
-  const [rate, setRate] = useState("");
+  const [service, setService] = useState("hairstyling"); // TODO: change workflow so this can filled 
+  const [provider, setProvider] = useState("Adam Young"); // TODO: fill in valid provider
+  const [time, setTime] = useState(null);
+  const [rate, setRate] = useState("");// TODO: change workflow so this can filled
+
+  useEffect(() => {
+    // TODO: make another endpoint to set a particular timeslot as booked - 
+    // TODO: get professional name from that endpoint too 
+    // TODO: get service name 
+    // TODO: get $ rate from same endpoint
+    // axios({
+    //   method: "GET", url: `http://localhost:5000/getAvailabilitySlotInfo?`,
+    //   headers: { 
+    //     professionalId: "36",
+    //     start: null,
+    //    }
+    // }).then((res) => {
+    //   // TODO set the time, with setTime
+    //   }).catch((err) => console.log(err));
+  
+  }, []);
+
+
+  const onConfirmation = () => {
+    // axios({
+    //   method: "POST", url: `http://localhost:5000/addBooking?`,
+    //   data: { 
+    //     professionalId: "36",
+    //     start: null,
+    //     end: null,
+    //     date: null
+    //   }
+    // }).then((res) => {
+    //   // TODO 
+    //   }).catch((err) => console.log(err));
+  }
+
+  const onMakeChanges = () => {
+    window.location = "/c/calendar/view"; // go back to booking
+  }
+
   return (
     <div className="page">
       <h2>Service</h2>
@@ -32,8 +69,8 @@ function BookingConfirmation() {
         <br />
         <br />
         <div className="btn-group">
-          <button type="submit">Confirm Details</button>
-          <button>Make Changes</button>
+          <button type="submit" onClick={onConfirmation}>Confirm Details</button>
+          <button onClick={onMakeChanges}>Make Changes</button>
         </div>
       </form>
     </div>
