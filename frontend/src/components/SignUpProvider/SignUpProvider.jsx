89dbd86a327443c040a1ff58062d434b199c96fb
Vishal Deb Sahoo <76522645+vishal-sahoo@users.noreply.github.com>
2022-07-12 19:10:17 -0400

    THEB-83 Change location to be a drop down in sign up.

--- frontend/src/components/SignUpProvider/SignUpProvider.jsx
+++ frontend/src/components/SignUpProvider/SignUpProvider.jsx
@@ -4,6 +4,12 @@ import "../Form.css";
 import "./SignUpProvider.css";
 import { Link, useNavigate } from "react-router-dom";
 import { getServices, signUpProvider } from "../../APICalls";
+import {
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Select as MUISelect,
+} from "@mui/material";
 
 function SignUpProvider() {
   const navigate = useNavigate();
@@ -110,14 +116,31 @@ function SignUpProvider() {
         value={signupForm.password}
         required
       />
-      <input
+      {/* <input
         onChange={handleChange}
         placeholder="Location"
         type="text"
         name="location"
         value={signupForm.location}
         required
-      />
+      /> */}
+      <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+        <InputLabel id="location-label">Location</InputLabel>
+        <MUISelect
+          labelId="location-label"
+          value={signupForm.location}
+          label="location"
+          name="location"
+          onChange={handleChange}
+        >
+          <MenuItem value={"Toronto, Ontario"}>Toronto, Ontario</MenuItem>
+          <MenuItem value={"Vaughn, Ontario"}>Vaughn, Ontario</MenuItem>
+          <MenuItem value={"Waterloo, Ontario"}>
+            Waterloo, Ontario
+          </MenuItem>
+        </MUISelect>
+      </FormControl>
+
       {servicesList && (
         <Select
           placeholder="Services Offered"
