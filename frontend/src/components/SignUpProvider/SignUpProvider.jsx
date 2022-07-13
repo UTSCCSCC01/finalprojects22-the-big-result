26c3eec839abb56946ba0e4718f310bade5e916e
AnanyaPoddar <ananyapoddar26@gmail.com>
2022-07-13 01:56:04 -0400

    THEB-83 Add form validation before descriptions popup

--- frontend/src/components/SignUpProvider/SignUpProvider.jsx
+++ frontend/src/components/SignUpProvider/SignUpProvider.jsx
@@ -6,21 +6,32 @@ import { Link, useNavigate } from "react-router-dom";
 import { getServices, signUpProvider } from "../../APICalls";
 import ServiceInfo from "../Profile/ServiceInfo";
 import {
+  TextField,
+  OutlinedInput,
+  Dialog,
+  DialogActions,
+  DialogTitle,
   MenuItem,
   InputLabel,
   FormControl,
   Select as MUISelect,
 } from "@mui/material";
-
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogTitle from "@mui/material/DialogTitle";
+import { ScriptElementKindModifier } from "typescript";
 
 function SignUpProvider() {
   const navigate = useNavigate();
   const [servicesList, setServicesList] = useState([]);
-  const [failedSignup, setFailedSignup] = useState(false);
+  const [errors, setErrors] = useState("");
+
+  const [signupForm, setSignupForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    servicesProvided: [],
+    location: "",
+    description: "",
+  });
 
   useEffect(() => {
     getServices().then((res) => {
@@ -35,18 +46,9 @@ function SignUpProvider() {
     });
   }, []);
 
-  const [signupForm, setSignupForm] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    servicesProvided: [],
-    location: "",
-    description: "",
-  });
-
   const handleSubmit = (e) => {
     e.preventDefault();
+    handleFormClose();
     signUpProvider({
       firstName: signupForm.firstName,
       lastName: signupForm.lastName,
@@ -62,7 +64,7 @@ function SignUpProvider() {
       })
       .catch((err) => {
         console.log(err);
-        setFailedSignup(true);
+        setErrors("User already exists");
       });
     //reset form after submission
     setSignupForm({
@@ -95,14 +97,19 @@ function SignUpProvider() {
   const [formOpen, setFormOpen] = useState(false);
   const [servicesDesc, setServicesDesc] = useState([]);
 
-  const handleFormOpen = () => {
-    setFormOpen(true);
-    //Set the initial values for the services selected
-    let servicesOffered = [];
-    signupForm.servicesProvided.forEach((svc) => {
-      servicesOffered.push({ service: svc, desc: "", price: 0 });
-    });
-    setServicesDesc(servicesOffered);
+  const handleFormOpen = (e) => {
+    e.preventDefault();
+    if (signupForm.servicesProvided.length === 0) {
+      setErrors("Must select at least one service");
+    } else {
+      setFormOpen(true);
+      //Set the initial values for the services selected
+      let servicesOffered = [];
+      signupForm.servicesProvided.forEach((svc) => {
+        servicesOffered.push({ service: svc, desc: "", price: 0 });
+      });
+      setServicesDesc(servicesOffered);
+    }
   };
 
   //Onchange to any of the new forms, update the servicesOffered descriptions and prices as required
@@ -121,102 +128,98 @@ function SignUpProvider() {
   };
 
   return (
-    <form id="signup-provider" className="form" onSubmit={handleSubmit}>
-      <input
-        onChange={handleChange}
-        placeholder="First name"
-        type="text"
-        name="firstName"
-        value={signupForm.firstName}
-        required
-      />
-      <input
-        onChange={handleChange}
-        placeholder="Last name"
-        type="text"
-        name="lastName"
-        value={signupForm.lastName}
-        required
-      />
-      <input
-        onChange={handleChange}
-        placeholder="Email"
-        type="email"
-        name="email"
-        value={signupForm.email}
-        required
-      />
-      <input
-        onChange={handleChange}
-        placeholder="Password (6 characters minimum)"
-        type="password"
-        name="password"
-        minLength={6}
-        value={signupForm.password}
-        required
-      />
-      {/* <input
-        onChange={handleChange}
-        placeholder="Location"
-        type="text"
-        name="location"
-        value={signupForm.location}
-        required
-      /> */}
-      <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
-        <InputLabel id="location-label">Location</InputLabel>
-        <MUISelect
-          labelId="location-label"
-          value={signupForm.location}
-          label="location"
-          name="location"
+    <>
+      <form id="signup-provider" className="form" onSubmit={handleFormOpen}>
+        <OutlinedInput
           onChange={handleChange}
-        >
-          <MenuItem value={"Toronto, Ontario"}>Toronto, Ontario</MenuItem>
-          <MenuItem value={"Vaughn, Ontario"}>Vaughn, Ontario</MenuItem>
-          <MenuItem value={"Waterloo, Ontario"}>
-            Waterloo, Ontario
-          </MenuItem>
-        </MUISelect>
-      </FormControl>
-      <br />
-      {servicesList && (
-        <Select
-          placeholder="Services Offered"
-          value={servicesList.filter((option) =>
-            signupForm.servicesProvided.includes(option.value)
-          )}
-          onChange={handleSelect}
-          isMulti
-          isClearable
-          options={servicesList}
+          placeholder="First name"
+          type="text"
+          name="firstName"
+          value={signupForm.firstName}
+          required
         />
-      )}
-      <textarea
-        placeholder="Description"
-        onChange={handleChange}
-        name="description"
-        value={signupForm.description}
-        rows="5"
-        cols="50"
-      />
-      <br />
-      {failedSignup && <p className="error">User already exists.</p>}
-      <button onClick={handleFormOpen}>Sign Up!</button>
-      <p>
-        Already have an account? <Link to="/login">Log In</Link>
-      </p>
-      <Dialog open={formOpen} onClose={handleFormClose} scroll="paper">
-        <DialogTitle>Enter Additional Information For Services</DialogTitle>
-        {signupForm.servicesProvided &&
-          signupForm.servicesProvided.map((arg) => (
-            <ServiceInfo addToServicesDesc={addToServicesDesc} service={arg} />
-          ))}
-        <DialogActions>
-          <Button onClick={handleSubmit}>Submit</Button>
-        </DialogActions>
-      </Dialog>
-    </form>
+        <OutlinedInput
+          onChange={handleChange}
+          placeholder="Last name"
+          type="text"
+          name="lastName"
+          value={signupForm.lastName}
+          required
+        />
+        <OutlinedInput
+          onChange={handleChange}
+          placeholder="Email"
+          type="email"
+          name="email"
+          value={signupForm.email}
+          required
+        />
+        <OutlinedInput
+          onChange={handleChange}
+          placeholder="Password (6 characters minimum)"
+          type="password"
+          name="password"
+          minLength={6}
+          value={signupForm.password}
+          required
+        />
+        <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
+          <InputLabel id="location-label">Location</InputLabel>
+          <MUISelect
+            labelId="location-label"
+            value={signupForm.location}
+            label="location"
+            name="location"
+            onChange={handleChange}
+          >
+            <MenuItem value={"Toronto, Ontario"}>Toronto, Ontario</MenuItem>
+            <MenuItem value={"Vaughn, Ontario"}>Vaughn, Ontario</MenuItem>
+            <MenuItem value={"Waterloo, Ontario"}>Waterloo, Ontario</MenuItem>
+          </MUISelect>
+        </FormControl>
+        <br />
+        {servicesList && (
+          <Select
+            placeholder="Services Offered"
+            value={servicesList.filter((option) =>
+              signupForm.servicesProvided.includes(option.value)
+            )}
+            onChange={handleSelect}
+            isMulti
+            isClearable
+            options={servicesList}
+          />
+        )}
+        <TextField
+          multiline
+          placeholder="Description"
+          onChange={handleChange}
+          name="description"
+          value={signupForm.description}
+          rows="5"
+        />
+        <br />
+        {errors && <p className="error">{errors}</p>}
+        {/* {failedSignup && <p className="error">User already exists.</p>} */}
+        <button type="submit">Sign Up!</button>
+        <p>
+          Already have an account? <Link to="/login">Log In</Link>
+        </p>
+        <Dialog open={formOpen} onClose={handleFormClose} scroll="paper">
+          <DialogTitle>Enter Additional Information For Services</DialogTitle>
+          {signupForm.servicesProvided &&
+            signupForm.servicesProvided.map((arg) => (
+              <ServiceInfo
+                addToServicesDesc={addToServicesDesc}
+                service={arg}
+              />
+            ))}
+          <DialogActions>
+            <button onClick={handleSubmit}>Submit</button>
+          </DialogActions>
+        </Dialog>
+      </form>
+    </>
   );
 }
 
